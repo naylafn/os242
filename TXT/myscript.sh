4a0f1305c04d4c576b54f07d609769d9478b125a
Nayla Farah <naylafarah.31@gmail.com>
2024-09-30 10:40:09 +0700

    WEEK04

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="81C6993909C2DBBE"
+REC2="E099A22EEA300322"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
